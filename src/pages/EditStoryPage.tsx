5a961287e436cf7e5de47f513177013070f823d4
Tegar Pamungkas <mtegarpamungkas111@gmail.com>
2024-08-22 16:06:55 +0700

    update

--- src/pages/EditStoryPage.tsx
+++ src/pages/EditStoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import EditStoryForm from "../components/EditStoryForm";
 import { Story } from "../types/story";
